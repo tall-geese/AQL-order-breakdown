fad3bf60c78e6836f33698ee278701dbe7f1478a
xyzMark <dieckman7461@gmail.com>
2021-06-23 22:18:26 -0400

    We're done verifying our query is correct, we're ready to do a bit of cleaning and then the analysis

---
+++ Notes.md
@@ -0,0 +1,34 @@
+### Notes
+
+The big question is can we simply take the AQL dedicated to the transaction for the 
+	Subassemblies and just use them as a placeholder for the tulips?
+	Or will we get different results if we break up the sub assem components?
+> We can't use the subassemblies as representative of the inssepctions on the components because we will often make them in different lot quantities. For example we'll make 500 tulips and do 29 inspections for that lot but they will be assigned to a job of 275 welded sub-assemblies.
+
+Can we use the ROUND(jh.OriginalProdQty * 1.1,0) instead of the jh.ProdQty?
+ProdQty doesnt dictate the amount of inspection we do in reality, however we are treating it this way
+in the query and if we scrapped a lot of parts mid-way this could throw us into a different AQL Bracket
+
+> Nope, nvm we're fine here. We dont actually apply the 10% when calculating the AQL
+
+In every PO we can have a lines broken up becuase
+they are a different length or because they are a 
+different diameter.
+
+> Can we find the minimum number of setups per Order?
+
+
+-----------------------------------------------------------
+
+### Cleaning TODO
+
+* For some reason double posistive assignments from a load ring to a total qty that doesnt make sense.
+	Were some of the parts damaged and simply needed new ones?
+	- **3563-05-1** (Yeah that seems likely. Best to remove the positive 7 qty.)
+* At least 3563-07-1 has MOD assemblies in it. This is giving non-shank components a false strong appearance here.
+  * Instead of simply filtering out the 'MOD' in the description here, we should first group by job number and determine which line items having less than 4 components. See if that aligns with our conclusions
+    * ~~3497-04-1 has 6~~ (they were neg quantities)
+    * ~~3500-10-1, 3500-10-2, 3500-11-1~~ (These are uniplanar assemblies, they do have shanks)
+    * **3563-07-1, 3563-08-1, 3563-08-2** (Mods)
+    * ~~3563-10-1 has 7~~?? (bunch of neg quantites, we filtered our now)
+    * ~~3574-18-1~~ and **3574-21-1** have 6 (one of the extensions acc. got assigned twice and with diff lot numbers so we can't UNIQUE filter, must erase this)
--- sql/InventoryAssignments.sql
+++ sql/InventoryAssignments.sql
@@ -1,14 +1,14 @@
 
 -- #AQL Inspections dedicated to the transaction Qty
-SELECT src2.*, (src2.TranQty/src2.QtyCompleted) * src2.AQLAmount[AQLforTranQty]
+SELECT DISTINCT src2.*, (src2.TranQty/src2.QtyCompleted) * src2.AQLAmount[AQLforTranQty]
 FROM (SELECT pt.JobNum, pt.LotNum, jh.DrawNum, pt.PartDescription, pt.TranQty, jh.QtyCompleted, 
 	CASE 
 		WHEN jh.PhaseID IN ('Tulip','Base','Reduct') THEN 'Tulip'
 		WHEN jh.PhaseID IN ('Shank','FShank') THEN 'Shank'
-		WHEN jh.PhaseID IN ('Clip','Slip') THEN 'Clip'
+		WHEN jh.PhaseID IN ('Clip','Slip', 'Solid', 'Split') THEN 'Clip/Split Ring'
 		WHEN jh.PhaseID IN ('Load') THEN 'Load Ring'
 		ELSE jh.PhaseID + '???'
-	END AS PhaseID,
+	END AS PhaseID, jh.OrigProdQty,
 			CASE
 				WHEN jh.OrigProdQty < 14 THEN jh.OrigProdQty 
 				WHEN jh.OrigProdQty < 151 THEN 13
@@ -20,43 +20,40 @@ FROM (SELECT pt.JobNum, pt.LotNum, jh.DrawNum, pt.PartDescription, pt.TranQty, j
 			END AS AQLAmount
 FROM EpicorLive10.dbo.PartTran pt
 Left outer join EpicorLive10.dbo.JobHead jh on pt.LotNum = jh.JobNum 
-WHERE pt.TranQty <> 0 and pt.JobNum like '3582%' AND jh.PhaseID NOT IN  ('Sub-Assy','Assy')
+WHERE pt.TranQty > 0 and pt.JobNum LIKE '{}%' AND jh.PhaseID NOT IN  ('Sub-Assy','Assy')
 and JH.PhaseID IS NOT NULL) src2
-ORDER BY src2.JobNum;
+UNION ALL
+SELECT DISTINCT src.*, (src.TranQty/src.QtyCompleted) * src.AQLAmount[AQLforTranQty]
+FROM (SELECT pt.JobNum ,jh2.JobNum[LotNum], jh2.DrawNum, jh2.PartDescription, pt.TranQty, jh2.QtyCompleted, 
+	CASE 
+		WHEN jh2.PhaseID IN ('Tulip','Base','Reduct') THEN 'Tulip'
+		WHEN jh2.PhaseID IN ('Extension','Towers') THEN 'Extension'
+		ELSE jh2.PhaseID + '???'
+	END AS PhaseID, jh2.OrigProdQty,
+			CASE
+				WHEN jh2.OrigProdQty < 14 THEN jh2.OrigProdQty 
+				WHEN jh2.OrigProdQty < 151 THEN 13
+				WHEN jh2.OrigProdQty < 281 THEN 20
+				WHEN jh2.OrigProdQty < 501 THEN 29 
+				WHEN jh2.OrigProdQty < 1201 THEN 34
+				WHEN jh2.OrigProdQty < 3201 THEN 42
+				ELSE 50
+			END AS AQLAmount
+FROM EpicorLive10.dbo.PartTran pt
+LEFT OUTER JOIN EpicorLive10.dbo.PartTran pt2 ON pt.LotNum = pt2.JobNum 
+LEFT OUTER JOIN EpicorLive10.dbo.JobHead jh ON jh.JobNum = pt2.JobNum 
+LEFT OUTER JOIN EpicorLive10.dbo.JobHead jh2 ON jh2.JobNum = pt2.LotNum 
+WHERE pt.TranQty > 0 and pt.JobNum LIKE '{}%' AND jh.PhaseID = 'Sub-Assy' AND pt2.TranType = 'STK-MTL' AND pt2.TranQty > 0)src
+ORDER BY JobNum;
 
 
+-----------------------------------------------------------------------------------------------------
 
-
-SELECT ld.JobNum, ld.OprSeq, ld.LaborHrs 
-FROM EpicorLive10.dbo.LaborDtl ld 
-WHERE (ld.OpCode = 'SWISS' OR ld.OpCode = 'FDINSP') AND ld.JobNum = 'NV14701'
-
-
--- Constructing the UNION Query (not yet finished)
--- Right now the Tran Qty is still showing the full assembly job, we care about the amount of that
--- job that has been issued to a line item
-SELECT src1.JobNum, src1.TranQty, src1.LotNum, pt2.TranQty, pt2.TranType
-FROM (SELECT pt.JobNum, pt.LotNum, jh.PhaseID,pt.TranQty
-FROM EpicorLive10.dbo.PartTran pt 
-LEFT OUTER JOIN EpicorLive10.dbo.JobHead jh ON jh.JobNum = pt.LotNum 
-WHERE pt.JobNum LIKE '3574%' and pt.TranQty <> 0 AND jh.PhaseID = 'Sub-Assy' ) src1
-LEFT OUTER JOIN EpicorLive10.dbo.PartTran pt2 ON src1.LotNum = pt2.JobNum
-WHERE pt2.TranType = 'STK-MTL'
-ORDER BY src1.JobNum
-
--- Ok, we need another column we can further clarify on, just select everything
-SELECT pt.JobNum, pt.LotNum, jh.PhaseID,pt.TranQty
-FROM EpicorLive10.dbo.PartTran pt 
-LEFT OUTER JOIN EpicorLive10.dbo.JobHead jh ON jh.JobNum = pt.LotNum 
-WHERE pt.JobNum LIKE '3574%' and pt.TranQty <> 0 AND jh.PhaseID = 'Sub-Assy'
-ORDER BY pt.JobNum 
-
--------------------------------------------------------------------------------------------------
 -- #AQL Inspections dedicated to the transaction Qty (GROUP BY PhaseID)
 --(Sum Tran Qty should be roughly the same for an order to be useable)
-SELECT Min(src2.LotNum)[LotNum], min(src2.DrawNum)[DrawNum], min(src2.PartDescription)[Description], sum(src2.TranQty)[SumTranQTY], sum(src2.QtyCompleted)[SumQtyCompleted],
+SELECT MAX(src2.JobNum)[JobNum], Min(src2.LotNum)[LotNum], min(src2.DrawNum)[DrawNum], min(src2.PartDescription)[Description], sum(src2.TranQty)[SumTranQTY], sum(src2.QtyCompleted)[SumQtyCompleted],
 	src2.PhaseID, sum((src2.TranQty/src2.QtyCompleted) * src2.AQLAmount)[SumAQLAttributed], sum(src2.AQLAmount)[SUMAQLAmountUpperLim], COUNT(*)[COUNT]
-from (SELECT pt.PartNum , pt.TranQty, jh.QtyCompleted, jh.ProdQty, pt.JobNum, pt.PartDescription, pt.LotNum, jh.OrigProdQty, jh.PhaseID, jh.DrawNum,
+from (SELECT pt.PartNum , pt.TranQty, jh.QtyCompleted, jh.ProdQty, pt.JobNum, pt.PartDescription, pt.LotNum, jh.OrigProdQty, jh.DrawNum,
 	CASE
 		WHEN jh.ProdQty < 14 THEN jh.ProdQty 
 		WHEN jh.ProdQty < 151 THEN 13
@@ -65,13 +62,20 @@ from (SELECT pt.PartNum , pt.TranQty, jh.QtyCompleted, jh.ProdQty, pt.JobNum, pt
 		WHEN jh.ProdQty < 1201 THEN 34
 		WHEN jh.ProdQty < 3201 THEN 42
 		ELSE 50
-	END AS AQLAmount
+	END AS AQLAmount,
+				CASE 
+					WHEN jh.PhaseID IN ('Tulip','Base','Reduct', 'Sub-Assy') THEN 'Tulip/Reduc'
+					WHEN jh.PhaseID IN ('Shank','FShank') THEN 'Shank'
+					WHEN jh.PhaseID IN ('Clip','Slip', 'Solid', 'Split') THEN 'Clip/Split Ring'
+					WHEN jh.PhaseID IN ('Load') THEN 'Load Ring'
+					ELSE jh.PhaseID + '???'
+				END AS PhaseID
 FROM EpicorLive10.dbo.PartTran pt
 Left outer join EpicorLive10.dbo.JobHead jh on pt.LotNum = jh.JobNum 
-WHERE pt.TranQty <> 0 and pt.JobNum like '3582%'  and jh.PhaseID <> 'Sub-Assy'
+WHERE pt.TranQty > 0 and pt.JobNum like '3561%'  --and jh.PhaseID <> 'Sub-Assy'
 --and JH.PhaseID IN ('Sub-Assy','screw','shank', 'tulip')
 and JH.PhaseID IS NOT NULL) src2
-GROUP BY src2.PhaseID
+GROUP BY src2.PhaseID;
 
 
 -----------------------------------------------------------------------------------------------------
@@ -81,8 +85,8 @@ SELECT max(pt.PartNum)[PartNum], sum(pt.TranQty)[sumTranQty], max(pt.JobNum)[Lin
 pt.LotNum, max(jh.ProdQty)[JH Qty],max(jh.QtyCompleted)[QtyCompleted], max(jh.PhaseID)[PhaseID], Count(*)[#TransactionsPerJob]
 FROM EpicorLive10.dbo.PartTran pt
 Left outer join EpicorLive10.dbo.JobHead jh on pt.LotNum = jh.JobNum 
-WHERE pt.TranQty <> 0 and pt.JobNum like '3582%'
+WHERE pt.TranQty <> 0 and pt.JobNum like '3497%'
 --and JH.PhaseID IN ('screw','shank','tulip')
 and JH.PhaseID IS NOT NULL
 GROUP BY pt.LotNum 
-Order by max(jh.PhaseID)
+Order by max(jh.PhaseID);
